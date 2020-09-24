a049c6171cdbc30dc0644ef092c84409f01b4fa4
codeGoblin <59504331+githubGoblin@users.noreply.github.com>
2020-09-24 16:25:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,6 @@
 const targetMap = new WeakMap()
-const depsMap = new Map()
 let activeEffect = null
-function effect(eff) {
-    activeEffect = eff
-    activeEffect()
-    activeEffect = null
-}
 
-let product = reactive({ price: 5, quantity: 2 })
-let total = 0
-let salePrice = 0
-effect(() => {
-    console.log('=> Total effect')
-    total = product.price * product.quantity
-    console.log('# new total ')
-    console.log(`-> ${total}\n`)
-})
-effect(() => {
-    console.log('=> salePrice effect')
-    salePrice = product.price * 0.9
-    console.log('# new salePrice')
-    console.log(`-> ${salePrice}\n`)
-})
 
 function track(target, key) {
     if (activeEffect) {
@@ -41,6 +20,7 @@ function track(target, key) {
     }
 }
 
+
 function trigger(target, key) {
     let depsMap = targetMap.get(target)
     if (!depsMap) {
@@ -53,18 +33,18 @@ function trigger(target, key) {
     }
 }
 
+
 function reactive(target) {
     const handler = {
         get(target, key, reciever) {
+            let result = Reflect.get(target, key, reciever)
             track(target, key)
-            console.log(`Get: ${key}.`)
-            return Reflect.get(target, key, reciever)
+            return result
         },
         set(target, key, value, reciever) {
             let oldValue = target[key]
             let result = Reflect.set(target, key, value, reciever)
             if (result && oldValue != value) {
-                console.log(`Set: ${key} -> ${value}`)
                 trigger(target, key)
             }
             return result
@@ -74,7 +54,48 @@ function reactive(target) {
 }
 
 
+function ref(raw) {
+    const r = {
+        get value() {
+            track(r, 'value')
+            return raw
+        },
+        set value(newVal) {
+            raw = newVal
+            trigger(r, 'value')
+        },
+    }
+    return r
+}
 
 
-product.quantity = 18
-product.price = 15
+function effect(eff) {
+    activeEffect = eff
+    activeEffect()
+    activeEffect = null
+}
+
+let product = reactive({ price: 5, quantity: 2 })
+let salePrice = ref(0)
+let total = 0
+
+effect(() => {
+    salePrice.value = product.price * 0.9
+})
+
+effect(() => {
+    total = salePrice.value * product.quantity
+})
+
+
+console.log(
+    `Before updated quantity total (should be 9) = ${total} salePrice (should be 4.5) = ${salePrice.value}`
+)
+product.quantity = 3
+console.log(
+    `After updated quantity total (should be 13.5) = ${total} salePrice (should be 4.5) = ${salePrice.value}`
+)
+product.price = 10
+console.log(
+    `After updated price total (should be 27) = ${total} salePrice (should be 9) = ${salePrice.value}`
+)
